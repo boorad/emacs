9b5723689559e64e423c3214eb2420aaf8d16d81
Brad Anderson <brad@sankatygroup.com>
2009-04-08 15:50:52 -0400

    see if we can get a good couchdb setup

---
+++ erlang/erlang_mode_couch_config.el
@@ -0,0 +1,40 @@
+(provide 'erlang_mode_couch_config)
+
+(add-to-list 'load-path "/opt/local/lib/erlang/lib/tools-2.6.2/emacs")
+(require 'erlang-start)
+(setq erlang-indent-level 4)
+(add-to-list 'exec-path "/opt/local/lib/erlang/bin")
+(setq erlang-root-dir "/opt/local/lib/erlang")
+
+(setq erlang-skel-mail-address "brad@sankatygroup.com")
+
+;; erl params for couchdb
+(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+(defun my-erlang-mode-hook ()
+  ;; prevent annoying hang-on-compile
+  (defvar inferior-erlang-prompt-timeout t)
+  ;; when starting an Erlang shell in Emacs, default in the node name
+  (setq inferior-erlang-machine-options
+        '("-sname" "couchdev"
+          "-smp" "auto" "errlog_type" "error" "+K" "true"
+          "-pa" "/Users/brad/dev/boorad/dbcore/src/couchdb"
+          "-pa" "/Users/brad/dev/boorad/dbcore/src/mochiweb"
+          "-pa" "/Users/brad/dev/boorad/dbcore/src/ibrowse"
+          "-dini" "/Users/brad/dev/boorad/dbcore/etc/couchdb/default_dev.ini"
+          "-lini" "/Users/brad/dev/boorad/dbcore/etc/couchdb/local_dev.ini"
+          "-pidfile" "/Users/brad/dev/boorad/dbcore/tmp/run/couchdb/couchdb.pid"
+          "-start" "couch_app"))
+
+  ;; tell distel to default to that node
+  (setq erl-nodename-cache
+	(make-symbol
+	 (concat
+	  "couchdev@"
+	  ;; MAC OS X uses "name.local" instead of "name," this should work
+	  ;; pretty much anywhere w/o having to much with NetInfo
+	  (car (split-string (shell-command-to-string "hostname"))))))
+
+  (define-key erlang-mode-map [f13]
+    (lambda () (interactive)
+      (progn
+	(my-erlang-compile)))))
--- my_config.el
+++ my_config.el
@@ -13,7 +13,9 @@
 (add-to-list 'load-path "~/dev/emacs/whitespace") ;; Whitespace package
 (add-to-list 'load-path "~/dev/emacs/autosave") ;; Autosave config
 
-(require 'erlang_mode_config) ;; Loading Erlang mode
+;(require 'erlang_mode_config) ;; Loading Erlang mode (dubdub & general config)
+(require 'erlang_mode_couch_config) ;; Loading Erlang mode (couchdb config)
+
 (require 'custom_keys_config) ;; custom key bindings
 (require 'flymake_config) ;; Loading flymake
 ;;(require 'git) ;; Loading git
@@ -23,6 +25,19 @@
 (require 'autosave_config) ;; Configures autosaving
 
 (distel-setup)
+
+;; tab lovin'
+(setq-default indent-tabs-mode nil) ; always replace tabs with spaces
+(setq-default tab-width 8) ; set tab width to 4 for all buffers
+
+;; refresh file
+;; from http://www.stokebloke.com/wordpress/2008/04/17/emacs-refresh-f5-key/
+(defun refresh-file ()
+  (interactive)
+  (revert-buffer t t t)
+  )
+
+(global-set-key [f5] 'refresh-file)
 
 
 ;; from Roberto Saccon
@@ -53,26 +68,6 @@
 	       (other
 		(message "Unexpected: %S" other)))))))))
 
-(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
-(defun my-erlang-mode-hook ()
-  ;; prevent annoying hang-on-compile
-  (defvar inferior-erlang-prompt-timeout t)
-  ;; when starting an Erlang shell in Emacs, default in the node name
-  (setq inferior-erlang-machine-options '("-sname" "boot"))
-  ;; tell distel to default to that node
-  (setq erl-nodename-cache
-	(make-symbol
-	 (concat
-	  "boot@"
-	  ;; MAC OS X uses "name.local" instead of "name," this should work
-	  ;; pretty much anywhere w/o having to much with NetInfo
-	  (car (split-string (shell-command-to-string "hostname"))))))
-
-  (define-key erlang-mode-map [f13]
-    (lambda () (interactive)
-      (progn
-	(my-erlang-compile)))))
-
 ;; just to illustrate how to use custom erlang compilation
 ;; from within other modes
 (add-hook 'foo-helper-mode-hook
@@ -86,4 +81,4 @@
 
 ;; final ui prefs
 (toggle-colors-black)
-;(split-window-horizontally)
+(split-window-horizontally)
